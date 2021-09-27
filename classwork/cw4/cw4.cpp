a86c4fc22ab291cda820e58bbc336b686941e5aa
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2021-09-27 19:22:22 +0300

    Classwork

---
+++ classwork/cw4/cw4.cpp
@@ -0,0 +1,15 @@
+//
+// Created by padjal on 27.09.21 г..
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Saluton mondo!";
+
+    string initial = "This is a new string";
+
+}
+
