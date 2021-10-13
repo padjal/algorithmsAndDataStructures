ed3c56806606bc9e95550bda2d6dd0f8f478c327
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2021-10-13 19:51:30 +0300

    Small progress on task A.

--- contests/contest6/A.cpp
+++ contests/contest6/A.cpp
@@ -1,6 +1,68 @@
+/**
+ * A program for combining two sorted linked lists.
+ */
+
 #include <iostream>
 
+class node{
+ public:
+    int data;
+    node* next;
+};
+
+class linkedList{
+ public:
+    node* head;
+    node* last;
+
+    void append(int number){
+        node* n = new node();
+
+        n->data = number;
+        n->next = nullptr;
+
+        if(head == nullptr){
+            head = n;
+        }
+
+        while(last->next != nullptr){
+            last = last->next;
+        }
+
+        last->next = n;
+    }
+
+    /**
+     * Prints out all elements of the current linked list.
+     */
+    void print(){
+
+    }
+};
+
+linkedList combine(linkedList* l1, linkedList* l2){
+    return;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m;
+
+    std::cin >> n >> m;
+
+    linkedList l1, l2;
+
+    for (int i = 0 ; i < n ; ++i) {
+        int newElement;
+        std::cin >> newElement;
+        l1.append(newElement);
+    }
+
+    for (int i = 0 ; i < m ; ++i) {
+        int newElement;
+        std::cin >> newElement;
+        l2.append(newElement);
+    }
+
+
     return 0;
 }
