a2759b529fa66da70b0502c7c7db6c5865d6c167
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2021-11-17 18:48:56 +0200

    Solved D

--- contests/contest8/TaskD.cpp
+++ contests/contest8/TaskD.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+template <class ValueType>
+class RBTree {
+public:
+    RBTree();
+
+    RBTree(const RBTree &other);
+
+    RBTree &operator=(const RBTree &other);
+
+    ~RBTree();
+
+    size_t size() const;
+
+    bool empty() const;
+
+    void insert(const ValueType &value);
+
+    void erase(const ValueType &value);
+
+    ValueType *lower_bound(const ValueType &value) const;  // NOLINT
+
+    ValueType *find(const ValueType &value) const;
+
+    ValueType *traversal() const;
+
+private:
+    // your code
+};
