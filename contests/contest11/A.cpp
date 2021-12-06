84149339c4ca3d33b70d50c4964a838a19384b1e
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2021-12-06 19:10:08 +0200

    daily progress

--- contests/contest11/A.cpp
+++ contests/contest11/A.cpp
@@ -1,6 +1,21 @@
+/*
+ * This program finds the minimum spanning tree.
+ */
+
 #include <iostream>
 
+class Edge {
+
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // User input.
+    int n, m;
+    std::cin >> n >> m;
+    // Check input.
+    if(n < 2 || n > 20000 || m < 1 || m > 100000){
+        return 0;
+    }
+
     return 0;
 }
