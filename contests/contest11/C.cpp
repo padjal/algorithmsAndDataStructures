0daf70bd161457354ed76d0579b248f6c08c8e65
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2021-12-09 00:21:42 +0200

    Contest 11 finished

--- contests/contest11/C.cpp
+++ contests/contest11/C.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+class BloomFilter {
+private:
+    size_t bits_;
+    size_t hashes_;
+
+public:
+    BloomFilter(size_t hashes, size_t bits) {
+        bits_ = bits;
+        hashes_ = hashes;
+    }
+
+    ~BloomFilter() {
+    }
+
+    // вставка информации об объекте
+    void add(const std::string &obj) {
+    }
+
+    // проверка принадлежности объекта
+    bool verify(const std::string &obj) {
+        return true;
+    }
+
+    // считаем относительную величину false positive rate по запросам тестирования
+    // принадлежности
+    double getFPRate() const {
+        return 0;
+    }
+
+    size_t numberOfHashFunctions() const {
+        return hashes_;
+    }
+
+    size_t numberOfBits() const {
+        return bits_;
+    }
+};
