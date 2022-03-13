4c9702f458c42c4e62134f2d2060aec282d46f66
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2022-03-13 12:19:47 +0200

    Revert "task a completed"
    This reverts commit 750ae2263d8474e103479feefa37705df2294c45.

--- contests/sem2/08_Prefix_and_Aho-Korasik/A.cpp
+++ contests/sem2/08_Prefix_and_Aho-Korasik/A.cpp
@@ -1,56 +1,6 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
-/*
- * Code attribution to https://cp-algorithms.com/string/prefix-function.html
- */
-
-std::vector<int> prefixFunction(std::string const s) {
-    size_t n = s.length();
-    std::vector<int> pi(n);
-    for (int i = 0; i < n; ++i) {
-        for (int k = 0; k <= i; ++k) {
-            if (s.substr(0, k) == s.substr(i - k + 1, k)) {
-                pi.at(i) = k;
-            }
-        }
-    }
-
-    return pi;
-}
-
-std::vector<int> optimizedPrefixFunction(std::string s) {
-    size_t n = s.length();
-    std::vector<int> pi(n);
-    for (size_t i = 1; i < n; ++i) {
-        int j = pi[i - 1];
-        while (j > 0 && s[i] != s[j]) {
-            j = pi[j - 1];
-        }
-        if (s[i] == s[j]) {
-            j++;
-        }
-        pi[i] = j;
-    }
-    return pi;
-}
-
-template <typename T>
-void printVector(std::vector<T> const &vector) {
-    for (auto elem : vector) {
-        std::cout << elem << " ";
-    }
-}
 
 int main() {
-    std::string input;
-
-    std::getline(std::cin, input);
-
-    auto res = optimizedPrefixFunction(input);
-
-    printVector(res);
-
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
